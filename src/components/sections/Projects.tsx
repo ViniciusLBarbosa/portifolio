d0d762011aba533c7de8cb803afe9fe6a9c0aedd
ViniciusLBarbosa <vinilimab@gmail.com>
2025-05-07 10:36:11 -0300

    little fix on projects...

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -16,7 +16,7 @@ type Project = {
 const projects: Project[] = [
   {
     title: 'FFXIV Complete Discord Bot',
-    description: 'A Discord bot for user registration and community engagement on a FFXIV Free Company.',
+    description: '\nA Discord bot for user registration and community engagement on a FFXIV Free Company.',
     image: '/imagemProjetos.png',
     technologies: ['Python' ],
     link: 'https://github.com/n0way02/ffxiv_discord_bot',
